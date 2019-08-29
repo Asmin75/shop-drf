b86e7bb64d50f93fc03e3710394542407d6518e9
Asmin75 <asminrai7@gmail.com>
2019-08-29 12:23:10 +0545

    model for shop created

---
+++ app/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class User(AbstractUser):
+    USER_TYPE_CHOICES =(
+        ('Agent', 'Agent'),
+        ('User', 'User'),
+    )
+    user_type = models.CharField(choices=USER_TYPE_CHOICES, max_length=100, default='Agent')
+    first_name = models.CharField(max_length=20, null=True, blank=True)
+    last_name = models.CharField(max_length=20, null=True, blank=True)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, null=True, blank=True)
+    password = models.CharField(max_length=255)
+    password2 = models.CharField(max_length=255)
+    address = models.CharField(max_length=20, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return self.username
+
+
+class Post(models.Model):
+    description = models.CharField(max_length=500)
+    preffered_location = models.CharField(max_length=150)
+    delivery_location = models.CharField(max_length=150)
+
+    def __str__(self):
+        return  self.delivery_location
+
+
+
+
+
